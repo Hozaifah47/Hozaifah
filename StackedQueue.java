dbf159c3873d055b5503e88b91131e95e2afafb0
Hozaifah47 <md.hozaifah1992008@gmail.com>
2025-10-12 22:24:20 +0530

    commit

--- StackedQueue.java
+++ StackedQueue.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class StackedQueue {
     Stack stack1;
     Stack stack2;
@@ -32,6 +34,40 @@ public class StackedQueue {
         } catch (Exception e) {
             throw new Exception("Queue underflow");
         }
+
+    }
+
+    public static void main(String[] args) {
+        int size, choice, num;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size");
+        size = sc.nextInt();
+        StackedQueue ob = new StackedQueue(size);
+
+        do {
+            System.out.println("Enter 0 to terminate the program 1 for Enqueue 2 for Dequque");
+            choice = sc.nextInt();
+            if (choice > 2 || choice < 0) {
+                System.out.println("Invalid Input!");
+                return;
+            }
+            try {
+                if (choice == 1) {
+                    System.out.println("Enter a number to Enqueue");
+                    num = sc.nextInt();
+                    ob.enqueue(num);
+                }
+                if (choice == 2) {
+                    System.out.println("Deququed: " + ob.dequeue());
+
+                }
+            } catch (Exception e) {
+
+                System.out.println("Error: " + e.getMessage());
+                return;
+            }
+
+        } while (choice != 0);
 
     }
 }
